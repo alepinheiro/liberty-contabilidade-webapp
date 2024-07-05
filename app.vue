a9ed050775a43445583287fb52536563f80e690a
Alessandro Pinheiro <alessandropsbra@gmail.com>
2024-07-05 20:52:16 -0300

    update nuxt config

--- app.vue
+++ app.vue
@@ -3,8 +3,14 @@
 </template>
 <script setup lang="ts">
 useHead({
-  title: "My App",
-  meta: [{ name: "description", content: "My amazing site." }],
+  title: "Liberty Contabilidade - (47) 98921-9131",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Na Contabilidade Liberty você encontra suporte especializado para sua contabilidade simplificada, livre de complicações e adaptada às suas necessidades pessoais e profissionais.",
+    },
+  ],
   bodyAttrs: {
     class: "test",
   },
